22879ca1a2d50f4ff50809e22c886b8a4db57c9f
michelle <foleymichelle9@gmail.com>
2020-01-30 09:29:51 -0700

    added yet another water fact

--- random.rb
+++ random.rb
@@ -3,3 +3,5 @@ Random Water Facts
 Men need to drink a gallon of water a day
 women need to drink 3/4 of a gallon of water a day
 Water is the most soluble substance
+this means that trace amount of drugs, debris, and waste are in
+tap water 
